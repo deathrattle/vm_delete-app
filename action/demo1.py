ba0f2fc47923b629e69e29cf138c7cfed3f75e59
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-27 18:59:21 +0530

    Update demo1.py

--- action/demo1.py
+++ action/demo1.py
@@ -1,50 +1,41 @@
-import sys
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.compute.models import DiskCreateOption
-
 from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
-    def run(self, SUBSCRIPTION_ID ,GROUP_NAME ,LOCATION , VM_NAME , client_id, secret, tenant ):
-       try:
-       		 SUBSCRIPTION_ID = '2f50f202-0a84-4c8c-a929-fcc5a3174590'
-		 GROUP_NAME = 'vm2'
-		 LOCATION ='West India'
-		 VM_NAME = 'vm2del'
+    def run(self, SUBSCRIPTION_ID, GROUP_NAME, LOCATION, VM_NAME, client_id, secret, tenant):
+        SUBSCRIPTION_ID = '2f50f202-0a84-4c8c-a929-fcc5a3174590'
+        GROUP_NAME = 'vm2'
+        LOCATION ='West India'
+        VM_NAME = 'vm2del'
+        
+        def get_credentials():
+            credentials = ServicePrincipalCredentials(
+                client_id = '7aae3fd9-9324-463f-89f5-7c4144971bfb',
+                secret = 'MJOJszdQ2fl]xYQQ5Lm+]hAM2vgR0e+5',
+                tenant = 'd5656af4-b7b3-45b9-9346-fb0547921fb7'
+            )
+        
+            return credentials
+        
+        def delete_resources(resource_group_client):
+            resource_group_client.resource_groups.delete(GROUP_NAME)
 
-			def get_credentials():
-    				credentials = ServicePrincipalCredentials(
-        			client_id = '7aae3fd9-9324-463f-89f5-7c4144971bfb',
-        			secret = 'MJOJszdQ2fl]xYQQ5Lm+]hAM2vgR0e+5',
-        			tenant = 'd5656af4-b7b3-45b9-9346-fb0547921fb7'
-    )
-
-    				return credentials
-
-		def delete_resources(resource_group_client):
-    resource_group_client.resource_groups.delete(GROUP_NAME)
-
-
-if __name__ == "__main__":
-                credentials = get_credentials()
-                resource_group_client = ResourceManagementClient(
-                credentials,
-                SUBSCRIPTION_ID
-                )              
-                network_client = NetworkManagementClient(
-                credentials,
-               SUBSCRIPTION_ID
-                )
-                compute_client = ComputeManagementClient(
-        credentials,
-        SUBSCRIPTION_ID
+        credentials = get_credentials()
+        resource_group_client = ResourceManagementClient(
+            credentials,
+            SUBSCRIPTION_ID
+        )
+        network_client = NetworkManagementClient(
+            credentials,
+            SUBSCRIPTION_ID
+        )
+        compute_client = ComputeManagementClient(
+            credentials,
+            SUBSCRIPTION_ID
         )
 
-                delete_resources(resource_group_client)
-
-       except requests.exceptions.MissingSchema:
-		 print("invalid")
-		 sys.exit(0)
+        delete_resources(resource_group_client)
